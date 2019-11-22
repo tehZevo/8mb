1964251452a17b2197945e15c9e98d808acdd5fc
tehZevo <tehzevo@users.noreply.github.com>
2019-11-22 16:11:57 -0500

    Initial commit

---
+++ 8mb.js
@@ -0,0 +1,74 @@
+var ffmpeg = require("fluent-ffmpeg");
+var youtubedl = require("youtube-dl");
+var moment = require("moment");
+var program = require("commander");
+var filenamify = require("filenamify");
+
+//8mb -u https://www.youtube.com/watch?v=oHg5SJYRHA0 -s 0:18 -e 0:36
+//node 8mb.js -u https://www.youtube.com/watch?v=oHg5SJYRHA0 -s 0:18 -e 0:36
+//node 8mb.js -u https://www.youtube.com/watch?v=td-qFfTig88 -s 0:08 -e 0:25
+//node 8mb.js -u https://www.youtube.com/watch?v=ShtIRODxvmc -s 0:06 -e 0:19
+//node 8mb.js -u https://www.youtube.com/watch?v=mj-v6zCnEaw -s 1:33 -e 1:57
+//node 8mb.js -u https://www.youtube.com/watch?v=4K7nJpH-B_s -s 0:07 -e 0:26
+program.version("0.0.0");
+program
+  .option("-s, --start-time <string>", "start time")
+  .option("-e, --end-time <string>", "end time")
+  .option("-u, --url <string>", "youtube url")
+  .option("-a, --audio-ratio <number>", "percentage of bitrate dedicated to audio", 1/4)
+  .option("-m, --megabytes <number>", "(hopefully) max filesize in MB", 8)
+  .option("-w, --max-width <number>", "width", 640)
+  .option("-h, --max-height <number>", "height", 480)
+
+program.parse(process.argv);
+var maxSize = program.megabytes * 1024 * 1024 //8mb
+var overhead = 0.1; //10% overhead
+var audioRatio = program.audioRatio;
+
+var url = program.url;
+var maxWidth = program.maxWidth;
+var maxHeight = program.maxHeight;
+var startTime = program.startTime;
+var endTime = program.endTime;
+
+async function main()
+{
+  //calculate duration
+  var t1 = moment(startTime, "mm:ss");
+  var t2 = moment(endTime, "mm:ss");
+  var duration = t2.diff(t1) / 1000;
+  var durationHMS = moment(duration * 1000).format("mm:ss");
+  //calculate bitrates
+  var bitrate = maxSize * (1 - overhead) / duration;
+  var abr = bitrate * audioRatio / 1024  * 8;
+  var vbr = bitrate * (1 - audioRatio) / 1024 * 8;
+  //download video
+  var video = youtubedl(url);
+
+  var info = await new Promise((res, rej) => video.on('info', (info) => res(info)));
+  var filename = filenamify(`${info.title}-${startTime}-${endTime}.mp4`, {replacement:"_"});
+
+  ffmpeg(video)
+    .setStartTime(startTime)
+    .setDuration(durationHMS)
+    .videoCodec("libx264")
+    .audioCodec("aac")
+    .format("mp4")
+    .videoBitrate(vbr)
+    .audioBitrate(abr)
+    .videoFilter(`scale=w=${maxWidth}:h=${maxHeight}:force_original_aspect_ratio=decrease`)
+    .save(filename)
+    .on('progress', function(progress) {
+      console.log('Processing: ' + progress.timemark);
+    })
+    .on("end", () =>
+    {
+      console.log("done")
+    })
+    .on("error", (err) =>
+    {
+      throw err;
+    });
+}
+
+main();
---
+++ README.md
@@ -0,0 +1,12 @@
+# 8mb.js
+
+Quickly clip YouTube videos at the specified timestamps
+
+# Installation
+* Probably requires FFmpeg to be installed
+* Probably also requires youtube-dl to be installed
+
+## Usage
+```
+# node 8mb.js -u https://www.youtube.com/watch?v=oHg5SJYRHA0 -s 0:19 -e 0:36
+```
