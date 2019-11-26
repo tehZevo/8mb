969b3b0c9040e246e81647fc8de0845b7cb7a53d
Grey <chronomly@gmail.com>
2019-11-25 20:29:40 -0500

    feat: add semi-working audio only option

--- 8mb.js
+++ 8mb.js
@@ -5,6 +5,7 @@ var youtubedl = require("youtube-dl");
 var moment = require("moment");
 var program = require("commander");
 var filenamify = require("filenamify");
+var package = require('./package.json');
 
 //8mb -u https://www.youtube.com/watch?v=oHg5SJYRHA0 -s 0:18 -e 0:36
 //node 8mb.js -u https://www.youtube.com/watch?v=oHg5SJYRHA0 -s 0:18 -e 0:36
@@ -12,7 +13,7 @@ var filenamify = require("filenamify");
 //node 8mb.js -u https://www.youtube.com/watch?v=ShtIRODxvmc -s 0:06 -e 0:19
 //node 8mb.js -u https://www.youtube.com/watch?v=mj-v6zCnEaw -s 1:33 -e 1:57
 //node 8mb.js -u https://www.youtube.com/watch?v=4K7nJpH-B_s -s 0:07 -e 0:26
-program.version("0.0.1");
+program.version(package.version);
 program
   .option("-s, --start-time <string>", "start time")
   .option("-e, --end-time <string>", "end time")
@@ -21,7 +22,7 @@ program
   .option("-m, --megabytes <number>", "(hopefully) max filesize in MB", 8)
   .option("-w, --max-width <number>", "width", 640)
   .option("-h, --max-height <number>", "height", 480)
-  .option("-a, --audio-only <bool>", "audio only", false);
+  .option("-ao, --audio-only <boolean>", "audio only", false);
 
 program.parse(process.argv);
 var maxSize = program.megabytes * 1024 * 1024 //8mb
@@ -50,9 +51,14 @@ async function main()
   var video = youtubedl(url);
 
   var info = await new Promise((res, rej) => video.on('info', (info) => res(info)));
-  var filename = filenamify(`${info.title}-${startTime}-${endTime}.mp4`, {replacement:"_"});
 
-  ffmpeg(video)
+  if (audioOnly === false) {
+    var filename = filenamify(`${info.title}-${startTime}-${endTime}.mp4`, {replacement:"_"});
+  } else if (audioOnly === true) {
+    var filename = filenamify(`${info.title}-${startTime}-${endTime}.mp3`, {replacement:"_"});
+  }
+
+  var command = new ffmpeg(video)
     .setStartTime(startTime)
     .setDuration(durationHMS)
     .videoCodec("libx264")
@@ -61,18 +67,24 @@ async function main()
     .videoBitrate(vbr)
     .audioBitrate(abr)
     .videoFilter(`scale=w=${maxWidth}:h=${maxHeight}:force_original_aspect_ratio=decrease`)
-    .save(filename)
     .on('progress', function(progress) {
       console.log('Processing: ' + progress.timemark);
     })
     .on("end", () =>
     {
-      console.log("All Done.")
+      console.log(`All Done.\nSaved ${filename}`)
     })
     .on("error", (err) =>
     {
       throw err;
     });
+    
+  if (audioOnly === true) {
+    command.noVideo()
+    command.save(filename)
+  } else {
+    command.save(filename)
+  }
 }
 
 main();
